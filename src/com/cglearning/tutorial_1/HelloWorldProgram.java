755833abb1a916daea6c0971dc4beb3d2033fff7
Sai Priya <Sai Priya@VENJYOSAI>
2020-08-24 09:32:55 +0530

    Tutorial 1 - Hello World

---
+++ src/com/cglearning/tutorial_1/HelloWorldProgram.java
@@ -0,0 +1,10 @@
+package com.cglearning.tutorial_1;
+
+public class HelloWorldProgram {
+
+	// main method is the starting point of the program
+	public static void main(String[] args) {
+		System.out.println("Hello World...!");
+	}
+
+}
