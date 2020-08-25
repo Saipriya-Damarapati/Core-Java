9f29465ae2981137f145d39d54a11d7960491c76
Sai Priya <Sai Priya@VENJYOSAI>
2020-08-25 11:01:03 +0530

    package-info

--- src/com/cglearning/tutorial_1/package-info.java
+++ src/com/cglearning/tutorial_1/package-info.java
@@ -2,8 +2,16 @@
  * Sample Hello World program
  * First Java Program
  */
+
 /**
  * @author Sai Priya
- *
+ * Reference: https://beginnersbook.com/java-tutorial-for-beginners-with-examples/
+ * 
+ * Pre-Learning: 
+ * https://beginnersbook.com/2013/05/java-introduction/
+ * https://beginnersbook.com/2013/05/jvm/
+ * 
+ * Relevant Links:
+ * https://beginnersbook.com/2013/05/first-java-program/
  */
 package com.cglearning.tutorial_1;
--- src/com/cglearning/tutorial_2/package-info.java
+++ src/com/cglearning/tutorial_2/package-info.java
@@ -1,8 +1,11 @@
 /**
- * 
+ * Variables in Java
+ * 1. Static variables, Instance Variables and Local Variables
  */
 /**
  * @author Sai Priya
- *
+ * 
+ * Relevant Links:
+ * https://beginnersbook.com/2017/08/variables-in-java/
  */
 package com.cglearning.tutorial_2;
