70693283549f0e17b86e6cb732091d72bc97ec23
Sai Priya <Sai Priya@VENJYOSAI>
2020-08-25 10:53:42 +0530

    package-info

---
+++ src/com/cglearning/tutorial_1/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Sample Hello World program
+ * First Java Program
+ */
+/**
+ * @author Sai Priya
+ *
+ */
+package com.cglearning.tutorial_1;
---
+++ src/com/cglearning/tutorial_2/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Sai Priya
+ *
+ */
+package com.cglearning.tutorial_2;
